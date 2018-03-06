c01e6fa64b75592d4dfae8a31fcd6e31fdebe17b
Mica Cardillo <micac@flowjo.com>
2018-03-05 23:12:17 -0800

    two rectangles with circles in upper left

--- client/src/index.js
+++ client/src/index.js
@@ -7,58 +7,81 @@ app.innerHTML = '<div id="log"></div>';
 
 window.onload = function () {
 
-    var width = self.frameElement ? 960 : innerWidth,
-    height = self.frameElement ? 500 : innerHeight;
+  function dragstarted() {
+    this.parentNode.appendChild(this);
 
-    var data = d3.range(2).map(function() { return [Math.random() * width, Math.random() * height]; });
+    d3.select(this).transition()
+        .duration(500)
+        .attr("r", 48);
+  }
 
-    var color = d3.scaleOrdinal(d3.schemeCategory10);
+  function dragged(d) {
+    d[0] = d3.event.x;
+    d[1] = d3.event.y;
+    console.log('d', d, 'd3.event', d3.event);
 
-    var drag = d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended);
+    d3.select(this)
+        .attr("transform", "translate(" + d + ")");
+  }
 
-    d3.select("body")
-        .on("touchstart", nozoom)
-        .on("touchmove", nozoom)
-      .append("svg")
-        .attr("width", width)
-        .attr("height", height)
+  function dragended() {
+    d3.select(this).transition()
+        .duration(500)
+        .attr("r", 32);
+  }
+
+  function nozoom() {
+    d3.event.preventDefault();
+  }
+
+  // const width = self.frameElement ? 400 : innerWidth;
+  // const height = self.frameElement ? 400 : innerHeight;
+
+  const width = 400;
+  const height = 400;
+
+  const rightData = d3.range(1).map(function() { return [width, height]; });
+  const rightControllerId = 'right-controller';
+
+  const leftData = d3.range(1).map(function() { return [width, height]; });
+  const leftControllerId = 'left-controller';
+
+  const drag = d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended);
+
+  d3.select("div")
+      .on("touchstart", nozoom)
+      .on("touchmove", nozoom)
+    .append("svg")
+      .attr("id", leftControllerId)
+      .attr("width", width)
+      .attr("height", height)
+      .attr("style", "border: 1px solid #000;")
       .selectAll("circle")
-        .data(data)
+      .data(leftData)
       .enter().append("circle")
-        .attr("transform", function(d) { return "translate(" + d + ")"; })
-        .attr("r", 32)
-        .style("fill", function(d, i) { return color(i); })
-        .call(drag);
+      .attr("r", 32)
+      .attr("style", "top: 50%; left: 50%;")
+      .style("fill", function(d, i) { return 'orange'; })
+      .call(drag);
 
-    function dragstarted() {
-      this.parentNode.appendChild(this);
-
-      d3.select(this).transition()
-          .duration(500)
-          .attr("r", 48);
-    }
-
-    function dragged(d) {
-      d[0] = d3.event.x;
-      d[1] = d3.event.y;
-      console.log('d', d, 'd3.event', d3.event);
-
-      d3.select(this)
-          .attr("transform", "translate(" + d + ")");
-    }
-
-    function dragended() {
-      d3.select(this).transition()
-          .duration(500)
-          .attr("r", 32);
-    }
-
-    function nozoom() {
-      d3.event.preventDefault();
-    }
+  d3.select("div")
+      .on("touchstart", nozoom)
+      .on("touchmove", nozoom)
+    .append("svg")
+      .attr("id", rightControllerId)
+      .attr("width", width)
+      .attr("height", height)
+      .attr("style", "border: 1px solid #000;")
+    .selectAll("circle")
+      .data(rightData)
+      .enter().append("circle")
+      .attr("r", 32)
+      .attr("style", "top: 50%; left: 50%;")
+      .style("fill", function(d, i) { return 'blue'; })
+      .call(drag);
 
     var conn;
     var log = document.getElementById("log");
